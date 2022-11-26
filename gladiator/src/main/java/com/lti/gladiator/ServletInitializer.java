75c5ca45e7b12cb506d5f5397278b307572af3ae
Shivam Singh <shivam.singh0706@gmail.com>
2022-11-26 16:51:17 +0530

    first commit

---
+++ gladiator/src/main/java/com/lti/gladiator/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lti.gladiator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GladiatorApplication.class);
+	}
+
+}
