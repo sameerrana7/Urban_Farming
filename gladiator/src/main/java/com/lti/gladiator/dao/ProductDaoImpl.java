fd9a14e573ec98f0327fcbfc20fd143d1f8816a3
Jaswanth-Reddy1 <saijaswanthreddyvaka111@gmail.com>
2022-11-28 16:01:49 +0530

    commiting

--- gladiator/src/main/java/com/lti/gladiator/dao/ProductDaoImpl.java
+++ gladiator/src/main/java/com/lti/gladiator/dao/ProductDaoImpl.java
@@ -2,26 +2,42 @@ package com.lti.gladiator.dao;
 
 import java.util.List;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 import com.lti.gladiator.beans.Product;
 
 public class ProductDaoImpl implements ProductDao{
-
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	
 	@Override
 	public List<Product> findProductByName(String productName) {
 		// TODO Auto-generated method stub
-		return null;
+		TypedQuery tqry = em.createQuery("Select e from Product e where e.productName = :productName",Product.class);
+		tqry.setParameter("productName", productName);
+		List<Product> proList = tqry.getResultList();
+		return proList;
 	}
 
 	@Override
 	public Product findProduct(int productId) {
 		// TODO Auto-generated method stub
-		return null;
+		Product p = em.find(Product.class,productId);
+		return p;
 	}
 
 	@Override
 	public List<Product> getAllProducts() {
 		// TODO Auto-generated method stub
-		return null;
+		Query qry = em.createQuery("select e from Product e");
+		List<Product> proList = qry.getResultList();
+		return proList;
 	}
 
 }
