0287f7252fc60b2d01074242a495c640425b3328
Jaswanth-Reddy1 <saijaswanthreddyvaka111@gmail.com>
2022-11-30 13:16:04 +0530

    dto methoda

--- gladiator/src/main/java/com/lti/gladiator/controllers/ProductController.java
+++ gladiator/src/main/java/com/lti/gladiator/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.lti.gladiator.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.gladiator.beans.Cart;
 import com.lti.gladiator.beans.Product;
+import com.lti.gladiator.beans.ProductDTO;
 import com.lti.gladiator.services.ProductService;
 import com.lti.gladiator.services.ProductServiceImpl;
 
@@ -25,34 +27,61 @@ public class ProductController {
 	ProductService productService;
 	
 	
-	public Product findProductByNo(int productId)
-	{
-		return productService.findProduct(productId);
-	}
-	
+//	public Product findProductByNo(int productId)
+//	{
+//		return productService.findProduct(productId);
+//	}
 	
 	//http://localhost:8282/products/productsbyname/pname
 	@GetMapping("/productsbyname/{pname}")
-	public List<Product> findAllByName(@PathVariable("pname")String productName)
+	public List<ProductDTO> findAllByName(@PathVariable("pname")String productName)
 	{
-		return productService.findProductByName(productName);
+		List<Product> pdList = productService.findProductByName(productName);
+		List<ProductDTO> pList = new ArrayList<>();
+		ProductDTO dto;
+		for(Product product : pdList)
+		{
+			dto = new ProductDTO();
+			
+			dto.productId = product.getProductId();
+			dto.productName = product.getProductName();
+			dto.productImage = product.getProductImage();
+			dto.productDesc = product.getProductDesc();
+			dto.productPrice = product.getProductPrice();
+			dto.productBrand = product.getProductBrand();
+			dto.productQty = product.getProductQty();
+			dto.categoryId = product.getCategory().getCategoryId();
+			dto.retailerId = product.getRetailer().getRetailerId();
+			pList.add(dto);
+		}
+		return pList;
 	}
-	
-	
 	//http://localhost:8282/products/allproducts
 	@GetMapping("/allproducts")
-	public List<Product> findAll()
+	public List<ProductDTO> findAll()
 	{
-		return productService.getAllProducts();
+		List<Product> pdList = productService.getAllProducts();
+		List<ProductDTO> pList = new ArrayList<>();
+		ProductDTO dto;
+		for(Product product : pdList)
+		{
+			dto = new ProductDTO();
+			
+			dto.productId = product.getProductId();
+			dto.productName = product.getProductName();
+			dto.productImage = product.getProductImage();
+			dto.productDesc = product.getProductDesc();
+			dto.productPrice = product.getProductPrice();
+			dto.productBrand = product.getProductBrand();
+			dto.productQty = product.getProductQty();
+			dto.categoryId = product.getCategory().getCategoryId();
+			dto.retailerId = product.getRetailer().getRetailerId();
+			pList.add(dto);
+		}
+		return pList;
 	}
 	
-	
-	//http://localhost:8282/products/cart
-	@PostMapping("/cart")
-	public int addToCart(@RequestBody Cart c)
-	{
-		return productService.addToCart(c);
-	}
+
 	
 	
 	
